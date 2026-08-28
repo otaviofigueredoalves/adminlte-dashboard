961f4c895d1828c7123edf414038673d51d3d642
otaviodev <117609558+otaviofigueredoalves@users.noreply.github.com>
2026-08-28 14:02:21 -0300

    feat: página de editar usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -15,5 +16,30 @@ class UserController extends Controller
         ];
         $users = User::paginate(5);
         return view('components.users.index', compact('users', 'config'));
+    }
+
+    public function edit(User $user)
+    {
+        $config = [
+            'title' => 'Configurações',
+            'hTitle' => 'Editar usuário',
+        ];
+        return view('components.users.edit', compact('user','config'));
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $input = $request->validate([
+            'email' => ['required','string','email','max:255', Rule::unique('users','email')->ignore($user)],
+            'password' => ['nullable','string','max:255'],
+        ]);
+
+        if(empty($input['password'])){
+            unset($input['password']);
+        }
+
+        $user->update($input);
+        return back()
+            ->with('success','Perfil atualizado com sucesso!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth'])->group(function () {
     Route::get('/',[HomeController::class,'index'])->name('home');
     Route::get('/usuarios',[UserController::class,'index'])->name('users.index');
+    Route::get('/usuarios/{user}/editar',[UserController::class,'edit'])->name('users.edit');
+    Route::put('/usuarios/{user}/editar',[UserController::class,'update'])->name('users.update');
 });
